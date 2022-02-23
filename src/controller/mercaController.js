7bfd66af2208d2b8ec6d24ce7f6ef876fa8751dd
Laudier2 <trabalhandooline2019@hotmail.com>
2022-02-23 00:03:48 -0300

    Isso é uma nova atualização

--- src/controller/mercaController.js
+++ src/controller/mercaController.js
@@ -3,27 +3,22 @@ import mercadopago from 'mercadopago';
 export const crearorden = async (req, res) => {
 
     mercadopago.configure({
-        access_token: 'TEST-3591372541539825-022300-64184a0fc109cd34234274b9aba574f8-1079041079'
+        access_token: process.env.TOKEN_MP
     });
 
     var preference = {
         items: [
             {
-                title: 'FoneAirdots',
+                title: 'Fone Airdots sem fio',
                 quantity: 1,
                 currency_id: 'ARS',
                 unit_price: 1.5
-            },
-            {
-                title: 'Celular',
-                quantity: 2,
-                currency_id: 'ARS',
-                unit_price: 10.5
             }
         ],
-        notification_url: "https://9bf9-2804-14c-bf3a-9071-50b6-2eb5-f64f-746.ngrok.io"
+        notification_url: "https://6cc3-2804-14c-bf3a-9071-50b6-2eb5-f64f-746.ngrok.io"
     };
-    console.log(preference)
+    //let compra = preference.items[0] == preference.items.currency_id ? preference.items[0] : preference.items[4];
+    //console.log(compra)
     mercadopago.preferences.create(preference)
     .then((ev) => {
         res.json(ev)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 import router from "./routes/index.js";
+import 'dotenv/config'
 
+const port = process.port || 3000 || 3001 || 3002
 const app = express();
 
 app.use(morgan('dev'));
 
 app.use(router)
-app.listen(3001)
+app.listen(port)
 
 //console.clear()
-console.log("http://localhost:3001")
+//console.log(process.env.TOKEN_MP)
+console.log(`http://localhost:${port}`)
